2f8a084e5138391fe73c2e90e01e72469ce918d2
Stewart <saxup@ucsc.edu>
2023-03-17 14:13:06 -0700

    imported assets scripts will be coming in next commit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-public class mainscene : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     public void PlayBlackjack()
     {
